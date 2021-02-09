87415edda17f3948a72060b818aa4968ee6539a2
Avinash Jain <57763597+logicbomb-1@users.noreply.github.com>
2021-02-09 12:18:07 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -23,5 +23,9 @@ document.getElementById("set").onclick = function() {
 document.getElementById("reset").onclick = function() {
 document.getElementById("data").innerText = "";
 chrome.storage.sync.clear();
+chrome.storage.sync.set({data: ["evilzone.org/*"]}, function() {
+      document.getElementById("data").innerText = "evilzone.org/*";
+}
+});
 window.close();
 }
