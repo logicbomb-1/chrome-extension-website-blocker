1f208d1d7e8a64b59879245b3301f4dca7db7d30
Avinash Jain <57763597+logicbomb-1@users.noreply.github.com>
2021-02-09 08:32:58 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,12 +4,7 @@ document.body.onload = function() {
   chrome.storage.sync.get("data", function(items) {
     if (!chrome.runtime.error) {
       console.log(items);
-      var str1 = items.toString(); 
-      var burl3 = str1.split(",");
-      console.log(burl3[1]);
-      //document.getElementById("data").innerText = items.data;
-            document.getElementById("data").innerText = burl3[1];
-
+      document.getElementById("data").innerText = items.data;
     }
   });
 }
